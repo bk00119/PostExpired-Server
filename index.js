cba445f0a7b11e6da2a30d21a9a933b9fc6124e1
Dr. Kim <briankim00119@gmail.com>
2022-07-08 17:15:29 -0400

    import path

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
 import cors from 'cors';
+import path from 'path';
 
 const app = express();
 const SEC_PORT = 443;
