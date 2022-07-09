e9f233f9eaf9e77c5a7ab5b6c51e00cb52943cb8
Dr. Kim <briankim00119@gmail.com>
2022-07-09 19:09:30 -0400

    test

--- controllers/users.js
+++ controllers/users.js
@@ -22,14 +22,14 @@ export const getUserByGoogleId = async(req, res) => {
     //FIX: USE TRY&CATCH INSTEAD OF IF&ELSE
     const { googId : _googId } = req.params;
     console.log(_googId);
-    const user = await user_schema.findOne({ "googleId" : _googId } , (error, user_data) => {
-        console.log(user_data);
-        // if(error) return res.status(500).json({ message : "error" });
-        if(user_data) {
-            res.status(200).json(user_data);
-        } else { //FIX: IF NOT FOUND RES.STATUS(409) WHY RES.STATUS(200)?
-            console.log(`not found ${_googId} on DB`);
-            res.status(200).json(user_data); 
-        }
-    });
+    // const user = await user_schema.findOne({ "googleId" : _googId } , (error, user_data) => {
+    //     console.log(user_data);
+    //     // if(error) return res.status(500).json({ message : "error" });
+    //     if(user_data) {
+    //         res.status(200).json(user_data);
+    //     } else { //FIX: IF NOT FOUND RES.STATUS(409) WHY RES.STATUS(200)?
+    //         console.log(`not found ${_googId} on DB`);
+    //         res.status(200).json(user_data); 
+    //     }
+    // });
 }
