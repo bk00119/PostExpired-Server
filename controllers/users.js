24f658c7260784da213fc8ea3541ecedbf853f75
Dr. Kim <briankim00119@gmail.com>
2022-07-14 15:56:04 -0400

    test

--- controllers/users.js
+++ controllers/users.js
@@ -3,21 +3,6 @@ import user_schema from '../models/newUser.js';
 import fs from 'fs';
 
 //POST
-const test = async(req, res) => {
-    console.log("testing...");
-    const user = new user_schema(req.body);
-    
-    try {
-        if(req.body.email && req.body.email!=undefined && req.body.email !=''){ //checking if there's any error in req.body
-            console.log(req.body.email,"created an account.");
-            await user.save(); //saves user data on DB
-        }
-        res.status(201).json(user); //201: created
-    } catch (error) {
-        res.status(409).json({ message : error.message });
-    }
-}
-
 export const createUser = async(req, res) => {
     console.log("creating...");
     const user = new user_schema(req.body);
@@ -27,11 +12,12 @@ export const createUser = async(req, res) => {
             console.log(req.body.email,"created an account.");
             await user.save(); //saves user data on DB
         }
+        console.log("success req");
         res.status(201).json(user); //201: created
     } catch (error) {
+        console.log("error");
         res.status(409).json({ message : error.message });
     }
-    // await test(req, res);
 }
 
 //GET
@@ -48,7 +34,6 @@ export const getUserByGoogleId = async(req, res) => {
             // res.status(200).json(user_data); 
             console.log(_googId);
             await createUser(req, res);
-            // test(req, res);
         }
     });
 }
