fd3d07299a64348f42e3ee83d1a31c7c87330b3a
Dr. Kim <briankim00119@gmail.com>
2022-07-14 15:51:23 -0400

    test

--- controllers/users.js
+++ controllers/users.js
@@ -19,18 +19,19 @@ const test = async(req, res) => {
 }
 
 export const createUser = async(req, res) => {
-    // const user = new user_schema(req.body);
+    console.log("creating...");
+    const user = new user_schema(req.body);
     
-    // try {
-    //     if(req.body.email && req.body.email!=undefined && req.body.email !=''){ //checking if there's any error in req.body
-    //         console.log(req.body.email,"created an account.");
-    //         await user.save(); //saves user data on DB
-    //     }
-    //     res.status(201).json(user); //201: created
-    // } catch (error) {
-    //     res.status(409).json({ message : error.message });
-    // }
-    await test(req, res);
+    try {
+        if(req.body.email && req.body.email!=undefined && req.body.email !=''){ //checking if there's any error in req.body
+            console.log(req.body.email,"created an account.");
+            await user.save(); //saves user data on DB
+        }
+        res.status(201).json(user); //201: created
+    } catch (error) {
+        res.status(409).json({ message : error.message });
+    }
+    // await test(req, res);
 }
 
 //GET
@@ -45,8 +46,8 @@ export const getUserByGoogleId = async(req, res) => {
         } else { //FIX: IF NOT FOUND RES.STATUS(409) WHY RES.STATUS(200)?
             // console.log(`not found ${_googId} on DB`);
             // res.status(200).json(user_data); 
-            console.log("creating...");
-            // createUser(req, res);
+            console.log("called...");
+            createUser(req, res);
             test(req, res);
         }
     });
